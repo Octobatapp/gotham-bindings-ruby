356b0654d2f7965f44ec006c6826432005d1d2d0
Gaultier Laperche <>
2022-09-15 12:10:53 +0200

    Moving DR12#document_request_id to the method prototype

--- lib/mirakl/requests/dr12.rb
+++ lib/mirakl/requests/dr12.rb
@@ -5,13 +5,12 @@ module Mirakl
     class DR12
       include Mirakl::ApiOperations::Request
 
-      def self.call(params = {}, opts = {})
-        if params[:document_request_id].blank?
+      def self.call(document_request_id, params = {}, opts = {})
+        if document_request_id.blank?
           raise ArgumentError,
-            "You must fill the `document_request_id` parameter to call the DR12 API endpoint"
+            "You must fill the `document_request_id` value to call the DR12 API endpoint"
         end
 
-        document_request_id = params.delete(:document_request_id)
         api_path = "document-request/#{document_request_id}/lines"
 
         resp, opts = request(:get, api_path, params, opts)
